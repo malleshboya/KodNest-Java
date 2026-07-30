5d1285ea8d8af7585d4845ff635bfbb65d569b41
BOYA MALLESH <malleshmalli8907@gmail.com>
2026-07-30 16:53:27 +0530

    Day1

---
+++ WelcomeToJava.java
@@ -0,0 +1,8 @@
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to the java track");
+        System.out.println("I'm from Bangalore");
+    }
+}
---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -0,0 +1,5 @@
+public class WelcomeToJava {
+    public static void main(String[] args) {
+       //Write your code here
+    }
+}
